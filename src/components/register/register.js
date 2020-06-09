0a979b5580caab93bcc27c410939288740b72ec3
Jan Napieralski <janek2000-2000@wp.pl>
2020-06-09 23:58:47 +0200

    register checkpoint

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -5,12 +5,20 @@ import {Link} from 'react-router-dom';
 
 class Register extends Component {
 
+
+
+    //pomoc w testach
+    componentDidMount(){
+        window.helpDel = this.helpDel
+    }
+
     state = {
         name:'',
         email:'',
         password:'',
         repPassword:'',
-        error:''
+        error:'',
+        accepted_policy : [false, false] 
     }
 
     handleChange = (e) => {
@@ -19,22 +27,131 @@ class Register extends Component {
         })
     }
 
+
+    //  ustawianie czy wymagane pola 'akceptuje blabla' są oba znaznaczone
+    handlePolicy = (e) =>{
+        let help = [...this.state.accepted_policy]
+        help[e] = (this.state.accepted_policy[e] == false) ? true : false
+        this.setState({
+            accepted_policy : help
+        })
+    }
+
+    helpDel = (email, password) =>{
+        fetch(`http://vps817819.ovh.net:50/users/?email=${email}&password=${password}`, {
+            method: "DELETE"
+        })
+        .then(response =>{
+            return response.json()
+        })
+        .then(data =>{
+            console.log(data)
+        })
+    }
+
     handleSubmit = (e) => {
         e.preventDefault();
         
-        const user = {
-            name:this.state.name,
-            email: this.state.email,
-            password: this.state.password,
+        let reg_mail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        let result_mail = this.state.email.match(reg_mail)
+
+        let reg_others = /(<|>|`|~|-|'|")/
+
+        // proste sprawdzenie poprawności formatu maila
+        if(result_mail == null){
+            this.setState({
+                error:'Podano zły format emaila',
+                name: ''
+            })
+
+            setTimeout(() => {
+                if(this.state.error){
+                    this.setState({
+                        error:''
+                    })
+                }
+            },3000)
         }
 
-        if(false){
-            //REJESTRACJA UDALO SIE
+        //proste sprawdzenie poprawności nazwy użytkownika (symboliczna ochrona przed XSS)
+        else if(this.state.name.match(reg_others) != null || this.state.password.match(reg_others) != null){
+            this.setState({
+                error:'Podano niedozwolone znaki <>`~-"\'',
+            })
 
-        } else {
-            //REJESTRACJA NIE UDALO SIE
+            setTimeout(() => {
+                if(this.state.error){
+                    this.setState({
+                        error:''
+                    })
+                }
+            },3000)
+        }
 
-        if(!this.state.error){
+        //sprawdzenie czy dwa hasła są takie same
+        else if(this.state.password != this.state.repPassword){
+            this.setState({
+                error:'Podane hasła nie są identyczne',
+            })
+
+            setTimeout(() => {
+                if(this.state.error){
+                    this.setState({
+                        error:''
+                    })
+                }
+            },3000)
+        }
+        else{
+            const user = {
+                name:this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+            }
+    
+            //sprawdzenie czy oba pola 'zgadzam się na warunki umowy blabla' są odznaczone
+            if(this.state.accepted_policy[0] && this.state.accepted_policy[1]){
+    
+                console.log(user)
+                fetch(`http://vps817819.ovh.net:50/users/?email=${user.email}&password=${user.password}&username=${user.name}`, {
+                    method: 'POST'
+                })
+                .then(response=>{
+                    return response.json()
+                })
+                .then(data=>{
+                    
+                    if(typeof(data) == "object"){
+                        sessionStorage.setItem('user_name', data.username)
+                        sessionStorage.setItem('user_id', data.id)
+                        sessionStorage.setItem('user_email', data.email)
+                    }
+    
+                    // BŁĄD albo 404 albo user istnieje czyli rejestracja nie udała się
+                    else{
+    
+                    }
+                })
+            }
+            else{
+                this.setState({
+                    error:'Proszę zaznaczyć politykę prywatności'
+                })
+    
+                setTimeout(() => {
+                    if(this.state.error){
+                        this.setState({
+                            error:''
+                        })
+                    }
+                },3000)
+            }
+        }
+        
+
+        
+
+        /*if(!this.state.error){
             this.setState({
                 error:'Koronawirus zabije nas wszystkiech'
             })
@@ -46,9 +163,9 @@ class Register extends Component {
                     })
                 }
             },3000)
-        }
+        }*/
             
-        }
+        
     }
 
 
@@ -94,14 +211,16 @@ class Register extends Component {
                         <div className="title-with-underline">
                             Zarejestruj się!
                         </div>
+
+                        {/* dodaj required do inputów, ja przy testach musiałem to usunąć */}
                         <input className="input-1" type="text" placeholder="Podaj swój nick/pseudonim..." name="name"
-                        required onChange={(e) => this.handleChange(e)}/>
+                         onChange={(e) => this.handleChange(e)}/>
                         <input className="input-1" type="mail" placeholder="Podaj swój email..." name="email"
-                        required onChange={(e) => this.handleChange(e)}/>
+                         onChange={(e) => this.handleChange(e)}/>
                         <input className="input-1" type="password" placeholder="Podaj nowe hasło..." name="password"
-                        required onChange={(e) => this.handleChange(e)}/>
+                         onChange={(e) => this.handleChange(e)}/>
                         <input className="input-1" type="password" placeholder="Powtórz hasło..." name="repPassword"
-                        required onChange={(e) => this.handleChange(e)}/>
+                         onChange={(e) => this.handleChange(e)}/>
                         <div className="captcha-fb-block">
                             <div className="captcha">
                                 -CAPTCHA-
@@ -114,7 +233,7 @@ class Register extends Component {
                         </div>
                         <div className="checkbox-and-text">
                             <div className="register-checkbox">
-                                <input className="register-checkbox" type="checkbox" name="regulations"/>
+                                <input className="register-checkbox" type="checkbox" name="regulations" onClick={()=>{this.handlePolicy(0)}}/>
                             </div>
                             <div className="text">
                                 <p>Akceptuje warunki umowy bla bla bla bla bal</p>
@@ -122,7 +241,7 @@ class Register extends Component {
                         </div>
                         <div className="checkbox-and-text">
                             <div className="register-checkbox">
-                                <input className="register-checkbox" type="checkbox" name="receive-not"/>
+                                <input className="register-checkbox" type="checkbox" name="receive-not" onClick={()=>{this.handlePolicy(1)}}/>
                             </div>
                             <div className="text">
                                 <p>Akceptuje warunki umowy bla bla bla bla bal asd sad asd asd as asd asd as</p>
